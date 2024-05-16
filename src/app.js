d99cc6ee3b87a6b1cae3661654afb20b6aaaa352
msmaple-dev <n8riemer@gmail.com>
2024-05-15 21:30:58 -0400

    Added Cooldowns & Tables Support for Sequelize

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-// Require the necessary discord.js classes
+const Sequelize = require('sequelize');
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
@@ -12,16 +12,39 @@ client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
+const tablesPath = path.join(__dirname, 'tables');
+const tablesFiles = fs.readdirSync(tablesPath).filter(file => file.endsWith('.js'));
+
+// const keyvPath = path.join(__dirname, 'keyv_stores');
+// const keyvFiles = fs.readdirSync(tablesPath).filter(file => file.endsWith('.js'));
+
+for (const file of tablesFiles) {
+	const filePath = path.join(tablesPath, file);
+	const table = require(filePath);
+	table.sync();
+}
+
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
 	// Set a new item in the Collection with the key as the command name and the value as the exported module
 	if ('data' in command && 'execute' in command) {
+		console.log(`Registered Command: ${command.data.name}`)
 		client.commands.set(command.data.name, command);
 	} else {
 		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 	}
 }
+
+
+
+// for (const file of keyvFiles) {
+// 	const filePath = path.join(tablesPath, file);
+// 	const keyv = require(filePath);
+// }
+
+client.cooldowns = new Collection();
+
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
@@ -38,6 +61,29 @@ client.on(Events.InteractionCreate, async interaction => {
 		console.error(`No command matching ${interaction.commandName} was found.`);
 		return;
 	}
+
+	const { cooldowns } = interaction.client;
+
+	if (!cooldowns.has(command.data.name)) {
+		cooldowns.set(command.data.name, 0);
+	}
+
+	const now = Date.now();
+	let timestamp = cooldowns.get(command.data.name);
+	const cooldownAmount = (command.cooldown ?? 0) * 1000;
+
+	if (timestamp) {
+		const expirationTime = timestamp + cooldownAmount;
+
+		if (now < expirationTime) {
+			const expiredTimestamp = Math.round(expirationTime / 1000);
+			return interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
+		}
+	}
+
+	timestamp = now;
+	setTimeout(() => cooldowns.delete(command.data.name), cooldownAmount);
+
 
 	try {
 		await command.execute(interaction);
