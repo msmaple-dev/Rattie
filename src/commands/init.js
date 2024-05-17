88eaa95fa2c4aeabbbb8c588e7e08ac965e12479
msmaple-dev <n8riemer@gmail.com>
2024-05-17 14:10:53 -0400

    Updated Init to use Identifier as field

--- src/commands/init.js
+++ src/commands/init.js
@@ -8,10 +8,10 @@ module.exports = {
 		.setName('init')
 		.setDescription('Adds you to init')
 		.addStringOption(option => option.setName('init').setDescription('Init Rolled').setRequired(true))
-		.addStringOption(option => option.setName('text').setDescription('Text w/ Init').setRequired(false)),
+		.addStringOption(option => option.setName('identifier').setDescription('Identifier for Additional Users Linked to You').setRequired(false)),
 	async execute(interaction) {
 		const initVal = interaction.options.getString('init');
-		const textVal = interaction.options.getString('text') ?? null;
+		const userIdentifier = interaction.options.getString('identifier') ?? null;
 		const channelId = interaction.channelId;
 		const userID = interaction.user.id;
 		const sqlID = BigInt(userID)
@@ -36,15 +36,15 @@ module.exports = {
 		if (await init_keyv.has(channelId)) {
 			let currentInit = await init_keyv.get(channelId).catch(err => interaction.reply(err));
 			let currentUsers = currentInit.users;
-			let matchingIndex = currentUsers.findIndex(usr => usr.userID === userID && usr.identifier === textVal)
+			let matchingIndex = currentUsers.findIndex(usr => usr.userID === userID && usr.identifier === userIdentifier)
 			let currentTurnUser = currentUsers[currentInit.currentTurn - 1]
 			if(matchingIndex >= 0){
 				let userInit = currentUsers[matchingIndex]
 				userInit.initVal = initVal
-				outputText = `Changed User ${textVal ? `${textVal} (<@${userID}>)` : `<@${userID}>`} Init to ${initVal}`
+				outputText = `Changed User ${userIdentifier ? `${userIdentifier} (<@${userID}>)` : `<@${userID}>`} Init to ${initVal}`
 			} else {
-				currentUsers.push({ userID: userID, identifier: textVal, initVal: initVal, decks: decks})
-				outputText = `Added User ${textVal ? `${textVal} (<@${userID}>)` : `<@${userID}>`} at Init ${initVal}`
+				currentUsers.push({ userID: userID, identifier: userIdentifier, initVal: initVal, decks: decks})
+				outputText = `Added User ${userIdentifier ? `${userIdentifier} (<@${userID}>)` : `<@${userID}>`} at Init ${initVal}`
 			}
 			currentUsers.sort((a, b) => (b.initVal - a.initVal))
 			currentInit.currentTurn = currentUsers.indexOf(currentTurnUser)+1
@@ -56,12 +56,12 @@ module.exports = {
 				currentTurn: 0,
 				round: 0,
 				users: [
-					{ userID: userID, identifier: textVal, initVal: initVal, decks: decks},
+					{ userID: userID, identifier: userIdentifier, initVal: initVal, decks: decks},
 				],
 				trackers: [],
 			};
 			await init_keyv.set(channelId, newInit)
-			outputText = `Started new Init with User ${textVal ? `${textVal} (<@${userID}>)` : `<@${userID}>`} at Init ${initVal}`
+			outputText = `Started new Init with User ${userIdentifier ? `${userIdentifier} (<@${userID}>)` : `<@${userID}>`} at Init ${initVal}`
 		}
 		await interaction.reply(outputText)
 	},
