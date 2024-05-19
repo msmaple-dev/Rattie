004b9898c29d9e3e29c74245cd6afc4f1c36ba5d
msmaple-dev <n8riemer@gmail.com>
2024-05-19 19:49:55 -0400

    Added forcing turn-end with /end true

--- src/commands/end.js
+++ src/commands/end.js
@@ -5,16 +5,18 @@ const nextTurn = require('../functions/init_utils');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('end')
-		.setDescription('Ends your turn'),
+		.setDescription('Ends your turn')
+		.addBooleanOption(option => option.setName('true').setDescription('End turns other than your own').setRequired(false)),
 	async execute(interaction) {
 		const channelId = interaction.channelId;
+		const endTrue = interaction.options.getBoolean('true') || false;
 		let outputText = ""
 
 		let currentInit = await init_keyv.get(channelId) ?? 0;
 
 		if(currentInit){
 			if(currentInit?.round >= 0 && currentInit.currentTurn >= 0){
-				if(currentInit.users[currentInit.currentTurn - 1].userID === interaction.user.id){
+				if(endTrue || currentInit.users[currentInit.currentTurn - 1].userID === interaction.user.id){
 					outputText = await nextTurn(channelId)
 				} else {
 					outputText = "It's not your turn!"
