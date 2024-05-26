8101490dc3b0fdbb3c8ab646cdac57d5ac187078
msmaple-dev <n8riemer@gmail.com>
2024-05-26 15:18:05 -0400

    Changed Rattie's kibble

--- src/commands/pet.js
+++ src/commands/pet.js
@@ -1,10 +1,12 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { weightedSelect } = require('../functions/roll_utils');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('pet')
 		.setDescription('Pet the rat'),
 	async execute(interaction) {
-		await interaction.reply('Yay :D');
+		let choices = {'Yay :D': 50, '*Bites You*': 1}
+		await interaction.reply(weightedSelect(choices));
 	},
 };
