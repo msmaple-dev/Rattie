3950479dd22eb0ca247f44b8d0c8dd531cf41b43
msmaple-dev <n8riemer@gmail.com>
2024-05-29 22:16:19 -0400

    Added multi-turn passing and skipping to end-of-round

--- src/commands/end.js
+++ src/commands/end.js
@@ -7,18 +7,22 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('end')
 		.setDescription('Ends your turn')
-		.addBooleanOption(option => option.setName('true').setDescription('End turns other than your own').setRequired(false)),
+		.addBooleanOption(option => option.setName('true').setDescription('End turns other than your own').setRequired(false))
+		.addIntegerOption(option => option.setName('turns').setDescription('Pass through the next X turns (also acts as end true)').setMinValue(0).setRequired(false))
+		.addBooleanOption(option => option.setName('endround').setDescription('Passes to start of next round (incompatible with turns)').setRequired(false)),
 	async execute(interaction) {
 		const channelId = interaction.channelId;
 		const endTrue = interaction.options.getBoolean('true') || false;
+		const count = interaction.options.getInteger('turns') || null;
+		const roundEnd = interaction.options.getBoolean('endround') || false;
 		let outputText = ""
 
 		let currentInit = await init_keyv.get(channelId) ?? 0;
 
 		if(currentInit){
 			if(currentInit?.round >= 0 && currentInit.currentTurn >= 0){
-				if(endTrue || currentInit.users[currentInit.currentTurn - 1].userID === interaction.user.id){
-					outputText = await nextTurn(channelId)
+				if(roundEnd || count || endTrue || currentInit.users[currentInit.currentTurn - 1].userID === interaction.user.id){
+					outputText = await nextTurn(channelId, (roundEnd ? -1 : (count || 1)))
 				} else {
 					outputText = "It's not your turn!"
 				}
--- src/functions/init_utils.js
+++ src/functions/init_utils.js
@@ -1,32 +1,41 @@
 const init_keyv = require('../keyv_stores/init_keyv');
 
-async function nextTurn(channelId){
+async function nextTurn(channelId, count = 1){
 	let currentInit = await init_keyv.get(channelId)
 	let returnText = ""
 	if(currentInit) {
 		// Trackers: userID, turnUserID, turnUserIdentifier, turns, content
-		if(currentInit.round <= 0){
-			currentInit.round = 1
-			currentInit.currentTurn = 1
-			returnText = `Combat Start!\nBeginning Round ${currentInit.round}\n`
-		} else {
-			currentInit.currentTurn += 1
-		}
-		if(currentInit.currentTurn > currentInit.users.length){
-			currentInit.round += 1
-			currentInit.currentTurn = 1
-			returnText += `Beginning Round ${currentInit.round}\n`
-		}
-		let turnUser = currentInit.users[currentInit.currentTurn - 1];
-		returnText += `Round ${currentInit.round}, ${turnUser.identifier ? `${turnUser.identifier} (<@${turnUser.userID}>)` : `<@${turnUser.userID}>`}'s Turn:`
+		// Checks for endRound via === -1
+		let effectiveCount = (count === -1 ? (currentInit.users.length - currentInit.currentTurn) + 1 : count)
+		for(let i = 0; i < effectiveCount; i++){
+			if(i > 0){
+				returnText += `\n`
+			}
+			if(currentInit.round <= 0){
+				currentInit.round = 1
+				currentInit.currentTurn = 1
+				returnText = `Combat Start!\nBeginning Round ${currentInit.round}\n`
+			} else {
+				currentInit.currentTurn += 1
+			}
+			if(currentInit.currentTurn > currentInit.users.length){
+				currentInit.round += 1
+				currentInit.currentTurn = 1
+				returnText += `\nBeginning Round ${currentInit.round}\n`
+			}
+			let turnUser = currentInit.users[currentInit.currentTurn - 1];
+			if(count > 0 || i === (effectiveCount - 1)){
+				returnText += `Round ${currentInit.round}, ${turnUser.identifier ? `${turnUser.identifier} (<@${turnUser.userID}>)` : `<@${turnUser.userID}>`}'s Turn:`
+			}
 
-		let trackers = currentInit.trackers.map(a => {return {...a, rounds: (turnUser.userID == a.turnUserID && turnUser.identifier == a.turnUserIdentifier ? a.rounds-1 : a.rounds)}})
+			let trackers = currentInit.trackers.map(a => {return {...a, rounds: (turnUser.userID == a.turnUserID && turnUser.identifier == a.turnUserIdentifier ? a.rounds-1 : a.rounds)}})
 
-		for (const tracker of trackers.filter(a => a.rounds <= 0)){
-			returnText += `\n<@${tracker.userID}>: ${tracker.content}`
+			for (const tracker of trackers.filter(a => a.rounds <= 0)){
+				returnText += `\n<@${tracker.userID}>: ${tracker.content}\n`
+			}
+			trackers = trackers.filter(a => a.rounds > 0)
+			currentInit.trackers = trackers
 		}
-		trackers = trackers.filter(a => a.rounds > 0)
-		currentInit.trackers = trackers
 		await init_keyv.set(channelId, currentInit)
 	} else {
 		returnText = "No Inits Entered!"
